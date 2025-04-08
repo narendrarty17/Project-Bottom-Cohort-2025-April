4711e690aadb720b84267fc0501f20673a0ae01b
narendrarty17 <61848600+narendrarty17@users.noreply.github.com>
2025-04-08 13:01:34 +0530

    section 2, skeleton

---
+++ 01/02_Final/.next/static/chunks/app_page_2686902b.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_2686902b.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_2686902b.js",
+  "chunks": [
+    "static/chunks/node_modules_next_dist_ea69acce._.js"
+  ],
+  "source": "dynamic"
+});
---
+++ 02/02_Final/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"R5YwVYY6m1dCS5eqtYIukDMnrsKdgZDug3+/0hfZAdI=\"\n}"
