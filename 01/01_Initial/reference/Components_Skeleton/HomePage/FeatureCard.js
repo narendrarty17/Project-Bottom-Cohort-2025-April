080ae26478a018b45bd1655d69a0b45a98075208
narendrarty17 <61848600+narendrarty17@users.noreply.github.com>
2025-04-14 23:02:06 +0530

    first session final

--- 01/01_Initial/reference/Components_Skeleton/HomePage/FeatureCard.js
+++ 01/01_Initial/reference/Components_Skeleton/HomePage/FeatureCard.js
@@ -1,9 +1,15 @@
 export default function FeatureCard({ image, title, description }) {
     return (
       <div className="bg-white rounded-xl shadow-md p-6 flex flex-col items-left text-left">
-        <img src={image} alt={title} className="w-16 h-16 mb-4" />
-        <h3 className="text-lg font-semibold text-gray-800 mb-2">{title}</h3>
-        <p className="text-sm text-gray-600">{description}</p>
+        
+        {/* Card Image */}
+        <img src="" alt={title} className="w-16 h-16 mb-4" />
+        
+        {/* Card Title */}
+        <h3 className="text-lg font-semibold text-gray-800 mb-2"></h3>
+        
+        {/* Card Description */}
+        <p className="text-sm text-gray-600"></p>
       </div>
     );
   }
